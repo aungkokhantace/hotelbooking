ba085e84055d16e5c97983612cd688fa841dbf72
Khin Zar Ni Wint <khinzarniwintt@gmail.com>
2017-11-16 13:14:11 +0630

    Modified secret key and publishable key

--- app/Payment/PaymentConstance.php
+++ app/Payment/PaymentConstance.php
@@ -8,27 +8,10 @@
  */
 class PaymentConstance
 {
-    //const STRIPE_SECRET_KEY         = "sk_test_Y1kf5chBWcPvsC2RBjVc1Ts9";
-    //const STIRPE_PUBLISHABLE_KEY    = "pk_test_fJWTQndbD4G5frZV8vxtFQiv";
-
-//    const STRIPE_SECRET_KEY         = "sk_test_pIwc9et6wlWYcAb2xfsz9UmY";
-//    const STIRPE_PUBLISHABLE_KEY    = "pk_test_1bnp84rAk5y91LIN9BJ7OdYX";
-    
-
-    const STRIPE_SECRET_KEY         = "sk_test_pfDJKF6zoTRgCuHdPptjcgQX";
-    const STIRPE_PUBLISHABLE_KEY    = "pk_test_4T1qgSnrYT8e6btfWGS7efld";
+    const STRIPE_SECRET_KEY         = "sk_test_wecHEoFknZ5MOpIgq0fqrx0R";
+    const STIRPE_PUBLISHABLE_KEY    = "pk_test_NTCDoN2guwqkNlVXBhWoTwbh";
     const STIRPE_CURRENCY           = "usd";
     const STIRPE_FEE_PERCENT        = 0.029; // 2.9%
-    const STRIPE_FEE_FIXED          = 0.3; // 30 cents
- 
-
-    /* For zarni */
-    /*
-    const STRIPE_SECRET_KEY         = "sk_test_Y1kf5chBWcPvsC2RBjVc1Ts9";
-    const STIRPE_PUBLISHABLE_KEY    = "pk_test_fJWTQndbD4G5frZV8vxtFQiv";
-    const STIRPE_CURRENCY           = "usd";
-    const STIRPE_FEE_PERCENT        = 0.029; // 2.9%
-    const STRIPE_FEE_FIXED          = 0.3; // 30 cents*/
-    
+    const STRIPE_FEE_FIXED          = 0.3; // 30 cents 
 
 }
