8737f2b5cfda70e1f541a28f9d909f10866f6517
aungkokhant <aungkokhantace@gmail.com>
2018-03-19 15:30:37 +0630

    fix form submitting issue in country list search

--- resources/views/backend/country/index.blade.php
+++ resources/views/backend/country/index.blade.php
@@ -104,6 +104,13 @@
                 } );
 
             });
+
+            $(window).keydown(function(event){
+              if(event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+              }
+            });
         });
     </script>
 @stop
