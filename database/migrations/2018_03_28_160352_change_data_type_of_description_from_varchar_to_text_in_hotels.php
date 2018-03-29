a46132ba4432b050cb919c9ee51a7295fa65868c
aungkokhant <aungkokhantace@gmail.com>
2018-03-29 09:25:32 +0630

    change data type of hotel description to long text

---
+++ database/migrations/2018_03_28_160352_change_data_type_of_description_from_varchar_to_text_in_hotels.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDataTypeOfDescriptionFromVarcharToTextInHotels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            $table->longText('description')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hotels', function (Blueprint $table) {
+            //
+        });
+    }
+}
