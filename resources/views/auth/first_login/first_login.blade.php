f63eddc70c2ca158222980b18b1a4b2b7bd5b9d5
aungkokhant <aungkokhantace@gmail.com>
2018-08-16 09:58:24 +0630

    remove forgot password link from backend first login

--- resources/views/auth/first_login/first_login.blade.php
+++ resources/views/auth/first_login/first_login.blade.php
@@ -58,7 +58,7 @@
                         <button type="submit" class="btn btn-default fill_color login_btn" name="login" style="background-color:#f37023";>LOG IN</button>
                     </div>
                     <div class="col-md-7">
-                            <a class="btn btn-link" href="{{ url('/password/reset') }}" style="color: #f37023";>Forgot Your Password?</a>
+                            <!-- <a class="btn btn-link" href="{{ url('/password/reset') }}" style="color: #f37023";>Forgot Your Password?</a> -->
 
                     </div>
                 </div>
